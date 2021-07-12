48a2eb5ec67a940be3e5e32dd450f886849b9591
Deron Smith <deron.brock@gmail.com>
2021-07-12 09:33:29 -0500

    updated paths for aws

--- paths.py
+++ paths.py
@@ -9,18 +9,19 @@ import os
 import re
 
 # If running on a local PC, use files from an external drive
-run_local = True
-local_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir))
+run_local = False
+#local_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir))
+local_dir = os.path.abspath(os.path.dirname(__file__))
 if run_local:
     root_dir = r"F:\opp-efed-data\scenarios"
     input_dir = r"F:\opp-efed-data\global"
 else:
-    root_dir = local_dir
-    input_dir = os.path.join(root_dir, "Input")
-local_dir = os.path.join(local_dir, "scenarios")
+    root_dir = os.getenv("SAM_INPUT_ROOT", local_dir)
+    input_dir = root_dir
+#local_dir = os.path.join(root_dir, "scenarios")
 
 # Root directories
-intermediate_dir = os.path.join(root_dir, "Intermediate")
+intermediate_dir = os.path.join(root_dir, "scenarios", "Intermediate")
 production_dir = os.path.join(root_dir, "Production")
 staged_dir = os.path.join(root_dir, "Staged")
 scratch_dir = os.path.join(root_dir, "Scratch")
@@ -58,7 +59,7 @@ nhd_map_path = os.path.join(table_path, "nhd_map.csv")
 
 # Misc paths
 shapefile_path = os.path.join(scratch_dir, "Shapefiles")
-remote_shapefile_path = os.altsep.join(("National", "Shapefiles"))
+remote_shapefile_path = os.sep.join(("National", "Shapefiles"))
 
 # Production data
 hydro_file_path = os.path.join(production_dir, "HydroFiles", "region_{}_{}.{}")  # region, type, ext
@@ -85,14 +86,14 @@ results_dir = os.path.join(pwc_selection_path, "national_summary_files", '{}_{}_
 ### AWS
 
 # Remote input data
-remote_nhd_path = os.altsep.join(("NHD", "NHDPlus{}", "NHDPlus{}"))  # vpu, region
-remote_cdl_path = os.altsep.join(("CDL", "r{}_{}.zip"))  # region, year
-remote_soil_path = os.altsep.join(("SSURGO", "gssurgo_g_{}.zip"))  # state
-remote_weather_path = os.altsep.join(("Weather", "region{}.zip"))  # region
-remote_table_path = os.altsep.join(("Parameters",))
+remote_nhd_path = os.sep.join(("NHD", "NHDPlus{}", "NHDPlus{}"))  # vpu, region
+remote_cdl_path = os.sep.join(("CDL", "r{}_{}.zip"))  # region, year
+remote_soil_path = os.sep.join(("SSURGO", "gssurgo_g_{}.zip"))  # state
+remote_weather_path = os.sep.join(("Weather", "region{}.zip"))  # region
+remote_table_path = os.sep.join(("Parameters",))
 
 # Remote production data
-remote_metfile_path = os.altsep.join(("WeatherArray", "region{}"))
-remote_hydrofile_path = os.altsep.join(("HydroFiles",))
-remote_recipe_path = os.altsep.join(("Recipes", "region_{}_{}.npz"))
-remote_scenario_path = os.altsep.join(("Scenarios", "region_{}.csv"))
+remote_metfile_path = os.sep.join(("WeatherArray", "region{}"))
+remote_hydrofile_path = os.sep.join(("HydroFiles",))
+remote_recipe_path = os.sep.join(("Recipes", "region_{}_{}.npz"))
+remote_scenario_path = os.sep.join(("Scenarios", "region_{}.csv"))
